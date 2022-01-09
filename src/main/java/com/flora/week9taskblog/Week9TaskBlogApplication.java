7de804901c2ad99347846a51a2d63fa1f32cd4ff
saltnlight <arongflorence@yahoo.com>
2022-01-09 19:20:40 +0100

    created Models - user, post, and comment.

---
+++ src/main/java/com/flora/week9taskblog/Week9TaskBlogApplication.java
@@ -0,0 +1,13 @@
+package com.flora.week9taskblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week9TaskBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week9TaskBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flora/week9taskblog/Week9TaskBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flora.week9taskblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week9TaskBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
