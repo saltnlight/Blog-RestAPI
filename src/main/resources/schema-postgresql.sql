7ee5764430fd990ab0042129e27b682408827840
saltnlight <arongflorence@yahoo.com>
2022-01-10 22:01:50 +0100

    created schema

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS Users;
+-- DROP TABLE IF EXISTS Users;
 
 CREATE TABLE IF NOT EXISTS Users (
                   id serial PRIMARY KEY,
@@ -12,18 +12,47 @@ CREATE TABLE IF NOT EXISTS Users (
                   deactivated bool NOT NULL,
                   deactivated_at timestamp NOT NULL
 );
--- FORIEGN ISS, VARCHAR LENGTH
+
+-- VARCHAR LENGTH
 CREATE TABLE IF NOT EXISTS Posts (
                   id serial PRIMARY KEY,
-                  title VARCHAR ( 50 ) UNIQUE NOT NULL,
-                  body VARCHAR ( 100000 ) NOT NULL,
-                  favourite bool NOT NULL,
-                  user_id pg_foreign_table REFERENCES Users NOT NULL
+                  user_id int8 NOT NULL,
+                  title VARCHAR ( 50 ) NOT NULL,
+                  body VARCHAR ( 255 ) NOT NULL,
+                  favorite bool NOT NULL,
+                  created_at timestamp NOT NULL,
+                  FOREIGN KEY (user_id) REFERENCES Users (id)
+);
+
+CREATE TABLE IF NOT EXISTS PostLikes (
+                  id serial PRIMARY KEY,
+                  user_id int8 NOT NULL,
+                  post_id int8 NOT NULL,
+                  FOREIGN KEY (post_id) REFERENCES Posts (id)
 );
 
 CREATE TABLE IF NOT EXISTS Comments (
-                id serial PRIMARY KEY,
-                body VARCHAR ( 50 ) NOT NULL,
-                user_id pg_foreign_table REFERENCES Users NOT NULL,
-                post_id pg_foreign_table REFERENCES Posts NOT NULL
+                 id serial PRIMARY KEY,
+                 user_id int8 NOT NULL,
+                 post_id int8 NOT NULL,
+                 body VARCHAR ( 50 ) NOT NULL,
+                 created_at timestamp NOT NULL,
+                 FOREIGN KEY (user_id) REFERENCES Users (id),
+                 FOREIGN KEY (post_id) REFERENCES Posts (id)
+);
+
+CREATE TABLE IF NOT EXISTS CommentLikes (
+                 id serial PRIMARY KEY,
+                 user_id int8 NOT NULL,
+                 comment_id int8 NOT NULL,
+                 FOREIGN KEY (user_id) REFERENCES Users (id),
+                 FOREIGN KEY (comment_id) REFERENCES Comments (id)
+);
+
+CREATE TABLE IF NOT EXISTS Connections (
+                 id serial PRIMARY KEY,
+                 user_id int8 NOT NULL,
+                 connection_id int8 NOT NULL,
+                 FOREIGN KEY (user_id) REFERENCES Users (id),
+                 FOREIGN KEY (connection_id) REFERENCES Users (id)
 );
